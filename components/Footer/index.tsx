75a077858bb86dc5b897ca9b3eb3c6bf3645ea8b
atishay <atishaykushwah91@gmail.com>
2024-12-25 01:14:02 +0530

    done

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,18 +7,18 @@ export default function Footer() {
     <div className={styles.footer}>
       <p>
         Made by{' '}
-        <a href='https://github.com/ayushiee' target='_blank' rel='noreferrer'>
-          <b>Ayushi Gupta</b>
+        <a href='https://github.com/atishayinfo' target='_blank' rel='noreferrer'>
+          <b>Atishay</b>
         </a>
       </p>
       <div>
-        <a href='https://linkedin.com/in/ayushiee/' className={styles.icon} target='_blank' rel='noreferrer'>
+        <a href='https://linkedin.com/in/atishaykushwah/' className={styles.icon} target='_blank' rel='noreferrer'>
           <Linkedin />
         </a>
-        <a href='https://twitter.com/ayushieee' className={styles.icon} target='_blank' rel='noreferrer'>
+        <a href='https://twitter.com/atihsayinfo' className={styles.icon} target='_blank' rel='noreferrer'>
           <Twitter />
         </a>
-        <a href='https://github.com/ayushiee' className={styles.icon} target='_blank' rel='noreferrer'>
+        <a href='https://github.com/atishayinfo' className={styles.icon} target='_blank' rel='noreferrer'>
           <Github />
         </a>
       </div>
